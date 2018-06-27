c923132bafe85fa5b7f244f0f8cecbdf7a9dbf3c
Unknown <19liukat@elmbrookstudents.org>
2018-06-27 11:04:23 -0500

    searching
    Repository and service methods

--- BoxProblem/BoxProblem/Repositories/BoxRepository.cs
+++ BoxProblem/BoxProblem/Repositories/BoxRepository.cs
@@ -1,9 +1,42 @@
-﻿using System;
+﻿using BoxProblem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
 namespace BoxProblem.Repositories
 {
     public class BoxRepository
     {
-           
+        private Data.ApplicationDbContext dbContext;
+
+        public BoxRepository(ApplicationDbContext context)
+        {
+        }
+
+        public List<BoxInventory> SearchWeight(int temp)
+        {
+            var results = dbContext.Boxes.Where(s => s.Weight == temp);
+            return results.ToList();
+        }
+
+        public List<BoxInventory> SearchVolume(int temp)
+        {
+            var results = dbContext.Boxes.Where(s => s.Volume == temp);
+            return results.ToList();
+        }
+
+        public List<BoxInventory> SearchCanHoldLiquid(bool temp)
+        {
+            var results = dbContext.Boxes.Where(s => s.CanHoldLiquid == temp);
+            return results.ToList();
+        }
+
+        public List<BoxInventory> SearchCost(double temp)
+        {
+            var results = dbContext.Boxes.Where(s => s.Cost == temp);
+            return results.ToList();
+        }
     }
 }
 
--- BoxProblem/BoxProblem/Services/BoxService.cs
+++ BoxProblem/BoxProblem/Services/BoxService.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using BoxProblem.Data;
+using BoxProblem.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace BoxProblem.Services
 {
     public class BoxService
     {
-        public BoxService()
+        private BoxRepository repository;
+
+        public BoxService(ApplicationDbContext context)
         {
+            repository = new BoxRepository(context);
         }
+
+        public List<BoxInventory> Search(int temp)
+        {
+            List<BoxInventory> result = repository.SearchWeight(temp);
+            List<BoxInventory> result1 = repository.SearchVolume(temp));
+            var finalResult = result.Union(result1);
+            return finalResult.ToList();
+        }
+
+        public List<BoxInventory> Search(bool temp)
+        {
+            return repository.SearchCanHoldLiquid(temp);
+        }
+
+        public List<BoxInventory> Search(double temp)
+        {
+            return repository.SearchCost(temp);
+        }
+
     }
 }
