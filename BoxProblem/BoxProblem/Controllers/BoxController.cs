34ed461aecbd44cb28ca66f472ff62e16989aee7
Unknown <19liukat@elmbrookstudents.org>
2018-06-28 09:14:05 -0500

    update

--- BoxProblem/BoxProblem/Controllers/BoxController.cs
+++ BoxProblem/BoxProblem/Controllers/BoxController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BoxProblem.Data;
+using BoxProblem.Services;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +12,27 @@ namespace BoxProblem.Controllers
 {
     public class BoxController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+
+        private BoxService service;
+        public ActionResult Index(int search1, bool? search2, string search3)
         {
-            return View();
+
+            List<BoxInventory> finalList = service.GetAllBoxes();
+            if (search1 != 0)
+            {
+                finalList = service.Search(search1);
+            }
+            if (search2 != null)
+            {
+                finalList = service.Search(search2);
+            }
+            if (search2 != null)
+            {
+                finalList = service.Search(search3);
+            }
+            return View(finalList);
+
         }
+
     }
 }
